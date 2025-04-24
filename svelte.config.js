a96f8cd41580c07d577e5672f974457b3146affd
kritam_al <thekritamkhatiwada@gmail.com>
2025-04-24 21:39:21 +0545

    finished with vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({
+		runtime:'nodejs18.x',
+		// split:false
+	}
+	) }
 };
 
 export default config;
